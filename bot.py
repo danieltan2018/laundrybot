fecf194c3596c10cee90b02f5bf78788e23ef196
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-02-12 14:17:26 +0800

    Add logic for wash complete

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # Dependency: pip install python-telegram-bot --upgrade
+import telegram
 import telegram.bot
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler,
@@ -8,6 +9,7 @@ from telegram.ext.dispatcher import run_async
 from flask import Flask, request
 import logging
 from datetime import datetime
+# Ensure secrets.py exists - refer to secrets_dummy.py format
 from secrets import bottoken
 import json
 
@@ -16,12 +18,16 @@ logging.basicConfig(filename='debug.log', filemode='a+', format='%(asctime)s - %
                     level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+# Define Flask webserver
 app = Flask(__name__)
+
+# Allows us to send messages without using context
+bot = telegram.Bot(token=bottoken)
 
 
 def loader():
     # Creates or loads json files in case of bot reboot
-    # Stores active users
+    # Stores all users' selected laundry room
     global users
     try:
         with open('users.json') as usersfile:
@@ -29,21 +35,35 @@ def loader():
     except:
         with open('users.json', 'w+'):
             users = {}
-    # Stores current queue
+    # Stores current watchers for each machine
+    global watch
+    try:
+        with open('watch.json') as watchfile:
+            watch = json.load(watchfile)
+    except:
+        with open('watch.json', 'w+'):
+            watch = {}
+    # Stores current queue as list of user IDs
     global queue
     try:
         with open('queue.json') as queuefile:
             queue = json.load(queuefile)
     except:
         with open('queue.json', 'w+'):
-            queue = {}
-    # Initialise all machines with state 2 (unknown if ON or OFF)
-    # Set last updated to current time
+            queue = []
+    # Load the machine database into memory
+    from parameters import machine_database
+    global rooms
+    rooms = set()
     global machines
     machines = {}
-    from parameters import machine_ids
-    for id in machine_ids:
-        machines[id] = {'state': 2, 'updated': datetime.now()}
+    for room in machine_database:
+        # Get all laundry rooms
+        rooms.add(room)
+        for washer in room:
+            # Initialise all machines with state -1 (unknown if ON or OFF)
+            # Set last updated to current time
+            machines[washer] = {'state': -1, 'updated': datetime.now()}
 
 # Run webserver to receive POSTs
 # Not recommended for production
@@ -55,51 +75,86 @@ def webserver():
 # TODO: Change to a secret route to prevent people from tampering
 @app.route('/', methods=['POST'])
 def postupdate():
-    # Incoming format is JSON e.g. {"id": "c1", "state": 0}
+    # Assume incoming format is JSON e.g. {"washer": "Cendana Washer 1", "state": 0}
     try:
         req_data = request.get_json()
-        id = req_data['id']
+        washer = req_data['washer']
         state = int(req_data['state'])
-        machines[id] = {'state': state, 'updated': datetime.now()}
-        machineupdate(id)
+        # Update master list of machine states
+        global machines
+        machines[washer] = {'state': state, 'updated': datetime.now()}
+        # Run necessary logic for users
+        machineupdate(washer, state)
         return ('Success', 200)  # HTTP success code
     except:
         return ('Error', 400)  # HTTP error code
 
 
-def machineupdate(id):
+def machineupdate(washer, state):
+    global machines
+    # Runs every time a machine changes state
+    # Get duration (experimental feature)
+    then = machines[washer]['updated']
+    now = datetime.now()
+    timer = then-now
+    duration = (timer.strftime(
+        "Your wash took %H hours, %M minutes and %S seconds."))
+    # Update master list of machine states
+    machines[washer] = {'state': state, 'updated': now}
+    # Notify user when done
+    global watch
+    if state == 0:
+        for id in watch[washer]:
+            msg = '*{}* has completed! _{}_'.format(washer, duration)
+            send(id, msg, [])
+    # Dispatch next in queue
+        global queue
+        # Remove and return first in line
+        id = queue.pop(0)
+        msg = "It's your turn! *{}* is now available.".format(washer)
+        send(id, msg, [])
+        # Add user to watch list
+        watch.setdefault(washer, []).append(id)  # create list if none exists
+        with open('watch.json') as watchfile:
+            json.dump(watch, watchfile)  # backup watch dictionary to file
+        msg = 'You will be notified automatically when your wash is done.'
+        send(id, msg, [])
     return
 
 # Send all messages via asynchronous function so main program does not wait for it
 @run_async
-def send(context, id, keyboard, msg):
+def send(id, msg, keyboard):
     # Convert list of InlineKeyboardButton to final format
     keyboard = InlineKeyboardMarkup(keyboard)
-    context.bot.send_message(
+    bot.send_message(
         chat_id=id, reply_markup=keyboard, text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
+    return
 
-
+# Start page for new users or when using /start
+@run_async
 def start(update, context):
-    user_id = str(update.message.chat_id)
+    id = str(update.message.chat_id)
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
     full_name = (str(first_name or '') + ' ' +
                  str(last_name or '')).strip()
-    msg = 'Hi {}, welcome to the *Laundry Bot*!\n\nWhat would you like to do?'.format(
+    msg = 'Hi *{}*, welcome to the Laundry Bot!\n\nWhat would you like to do?'.format(
         full_name)
+    keyboard = [
+        [InlineKeyboardButton(
+            "Check Available Washers", callback_data='available')],
+        [InlineKeyboardButton(
+            "Notify when Done", callback_data='notify')],
+        [InlineKeyboardButton(
+            "Join Queue", callback_data='reserve')]
+    ]
+    send(id, msg, keyboard)
+    msg = "_You can type a message at any time to send feedback to the admins, or type /start to return to this page._"
+    send(id, msg, [])  # Call function with empty list if no buttons
 
 
+### FOR REFERENCE ONLY ###
 '''
-def groupinit(context):
-    global groups
-    for group in groups:
-        groups[group] = str(message.message_id)
-        with open('groups.json', 'w') as groupfile:
-            json.dump(groups, groupfile)
-        groupedit(context)
-        break
-
-
 def groupedit(context):
     for key, value in groups.items():
         context.bot.edit_message_text(
@@ -109,6 +164,7 @@ def groupedit(context):
             parse_mode=telegram.ParseMode.HTML
         )
 '''
+### FOR REFERENCE ONLY ###
 
 
 def main():
--- parameters.py
+++ parameters.py
@@ -1,2 +1,9 @@
-# List of all machine IDs
-machine_ids = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6']
+# Dictionary of laundry rooms and their machines
+machine_database = {'Cendana': {
+    'Cendana Washer 1',
+    'Cendana Washer 2',
+    'Cendana Washer 3',
+    'Cendana Washer 4',
+    'Cendana Washer 5',
+    'Cendana Washer 6'}
+}
